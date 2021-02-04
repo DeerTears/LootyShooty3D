0da54de40dd0e0ce5b60e75a008f919d6c3d4a15
Ember Bland <gdnghtgrl@gmail.com>
2021-02-03 18:17:15 -0800

    fix main menu

--- control/menus/mainmenu.gd
+++ control/menus/mainmenu.gd
@@ -1,40 +1,28 @@
 extends Panel
 
-onready var play_romp = $MarginContainer/VBoxContainer/Grid/Romp
-onready var play_pits
-onready var play_keep
-onready var about = $MarginContainer/VBoxContainer/Grid/About
-onready var quit = $MarginContainer/VBoxContainer/Grid/Quit
-onready var settings = $MarginContainer/VBoxContainer/Grid/Settings
-onready var itch_link = $MarginContainer/VBoxContainer/PanelContainer2/HBoxContainer/LinkButton
+onready var start = $MarginContainer/VBoxContainer/Buttons/Start
+onready var settings = $MarginContainer/VBoxContainer/Buttons/Settings
+onready var quit = $MarginContainer/VBoxContainer/Buttons/Quit
+
+onready var itch_link = $MarginContainer/VBoxContainer/Right/MoreGames
+onready var patch_notes = $MarginContainer/VBoxContainer/Left/VBoxContainer/HBoxContainer/PatchNotes
+onready var patch_icon = $MarginContainer/VBoxContainer/Left/VBoxContainer/HBoxContainer/PatchIcon
 
 func _ready():
-	if MenuMusic.is_playing() == false:
-		MenuMusic.play()
-	play_romp.connect("pressed",self,"play_level",["romp"])
-#	play_pits.connect("pressed",self,"play_level",["pits"])
-#	play_keep.connect("pressed",self,"play_level",["keep"])
-	settings.connect("pressed",get_tree(),"change_scene",["res://menus/settings.tscn"])
-	about.connect("pressed",get_tree(),"change_scene",["res://menus/about.tscn"])
+	start.connect("pressed",self,"play_level",["barracks"])
+	start.grab_focus()
+	settings.connect("pressed",get_tree(),"change_scene",["res://control/menus/settings.tscn"])
 	quit.connect("pressed",get_tree(),"quit")
+	
 	itch_link.connect("pressed",self,"open_link",["https://deertears.itch.io"])
-	$MarginContainer/VBoxContainer/Grid/Romp.grab_focus()
+	patch_notes.connect("pressed",self,"open_patch_notes")
+	patch_icon.connect("pressed",self,"open_patch_notes")
 
 func open_link(url:String):
 	OS.shell_open(url)
 
 func play_level(level:String):
-	MenuMusic.stop()
-	match level:
-		"romp":
-			GameInfo.ingame = true
-			GameInfo.switch_gamestate(GameInfo.GameStates.LOADING)
-			get_tree().change_scene("res://levels/romp_multi.tscn")
-#		"pits":
-#			GameInfo.ingame = true
-#			GameInfo.switch_gamestate(GameInfo.GameStates.LOADING)
-#			get_tree().change_scene("res://levels/2pits.tscn")
-#		"keep":
-#			GameInfo.ingame = true
-#			GameInfo.switch_gamestate(GameInfo.GameStates.LOADING)
-#			get_tree().change_scene("res://levels/keep.tscn")
+	get_tree().change_scene("res://levels/%s.tscn" % [level])
+
+func open_patch_notes():
+	pass
